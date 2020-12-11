44bcfe8e1403a5de4e4856e7aba32c24556d4466
h-salman <42090002+h-salman@users.noreply.github.com>
2020-12-11 08:53:16 +0100

    Completed optional exercise

--- machine-learning-ex5/ex5/optionalExercise.m
+++ machine-learning-ex5/ex5/optionalExercise.m
@@ -1,5 +1,9 @@
 %% Machine Learning online class
 % Optional ungraded Exercise
+
+%% Initialization
+clear ; close all; clc
+
 % Load from ex5data1: 
 % You will have X, y, Xval, yval, Xtest, ytest in your environment
 load ('ex5data1.mat');
@@ -24,8 +28,31 @@ X_poly_val = bsxfun(@rdivide, X_poly_val, sigma);
 X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
 
 %Choose a value for lambda
-lambda = 1;
-num_samples = 7; %take seven samples 
+lambda = 0.01;
+
+%Create a vector with different num samples
+%Ranging from 1 percent of the dataset to 100% of the dataset
+
+num_samples = zeros(10,1);
+
+for i=1:10
+	num_samples(i) = floor((i/10)*size(X,1)); %take 80% of the data as num_samples
+end
+
 num_iter = 50;
 
+error_train = zeros(10,1);
+error_val = zeros(10,1);
+%Get the average error over the training set and validation set
+for i=1:10
+	[error_train(i), error_val(i)] = ...
+		randomSampleValidation(X_poly, y, X_poly_val, yval, lambda, ...
+		num_samples(i), num_iter);
+end
 
+%Plot the curves
+figure(1);
+plot(num_samples, error_train, num_samples, error_val);
+xlabel('Number of training examples')
+ylabel('Error')
+legend('Train', 'Cross Validation')
--- machine-learning-ex5/ex5/randomSampleValidation.m
+++ machine-learning-ex5/ex5/randomSampleValidation.m
@@ -1,5 +1,5 @@
 function [error_train, error_val] = ...
-	randomSampleValidation(X, y, Xval, yVal, lambda, num_samples, num_iter)
+	randomSampleValidation(X, y, Xval, yval, lambda, num_samples, num_iter)
 	
 % RANDOMSAMPLEVALIDATION Choose random samples from X, y , Xval and yVal to
 % calculate the error. num_samples number of samples will be chosen randomly
@@ -11,21 +11,26 @@ m = size(y);
 error_train = 0;
 error_val = 0;
 
-for i=1:num iter
+for i=1:num_iter
+	%Select num_samples randomly from X_poly and X_val
+	train_indices = randperm(m, num_samples); 
+	X_train = X(train_indices, :);
+	y_train = y(train_indices);
+	Xval_train = Xval(train_indices, :);
+	yval_train = yval(train_indices);
 	
+	%Get theta by training the model
+	theta_train = trainLinearReg(X_train, y_train, lambda);
+	%Accumulate the error from the trained theta
+	error_train += linearRegCostFunction(X_train, y_train, theta_train, 0); %cost without regularization
+	
+	%Calculate the cost of the validation set
+	error_val += linearRegCostFunction(Xval_train, yval_train, theta_train, 0);
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
+%Calculate the average error over all iterations
+error_train = error_train/num_iter;
+error_val = error_val/num_iter;
 
 
 end

96ac8eab9cddec6441237120ef366b00f75bf623
h-salman <42090002+h-salman@users.noreply.github.com>
2020-12-11 08:13:05 +0100

    Completed Exercise 5

---
+++ machine-learning-ex5/ex5/optionalExercise.m
@@ -0,0 +1,31 @@
+%% Machine Learning online class
+% Optional ungraded Exercise
+% Load from ex5data1: 
+% You will have X, y, Xval, yval, Xtest, ytest in your environment
+load ('ex5data1.mat');
+
+p=8;
+m = size(y);
+% Map X onto Polynomial Features and Normalize
+X_poly = polyFeatures(X, p);
+[X_poly, mu, sigma] = featureNormalize(X_poly);  % Normalize
+X_poly = [ones(m, 1), X_poly];    
+
+% Map X_poly_test and normalize (using mu and sigma)
+X_poly_test = polyFeatures(Xtest, p);
+X_poly_test = bsxfun(@minus, X_poly_test, mu);
+X_poly_test = bsxfun(@rdivide, X_poly_test, sigma);
+X_poly_test = [ones(size(X_poly_test, 1), 1), X_poly_test];         % Add Ones
+
+% Map X_poly_val and normalize (using mu and sigma)
+X_poly_val = polyFeatures(Xval, p);
+X_poly_val = bsxfun(@minus, X_poly_val, mu);
+X_poly_val = bsxfun(@rdivide, X_poly_val, sigma);
+X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
+
+%Choose a value for lambda
+lambda = 1;
+num_samples = 7; %take seven samples 
+num_iter = 50;
+
+
---
+++ machine-learning-ex5/ex5/randomSampleValidation.m
@@ -0,0 +1,31 @@
+function [error_train, error_val] = ...
+	randomSampleValidation(X, y, Xval, yVal, lambda, num_samples, num_iter)
+	
+% RANDOMSAMPLEVALIDATION Choose random samples from X, y , Xval and yVal to
+% calculate the error. num_samples number of samples will be chosen randomly
+% The average of both errors will be taken from num_iter
+
+%sum useful 
+m = size(y);
+
+error_train = 0;
+error_val = 0;
+
+for i=1:num iter
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
